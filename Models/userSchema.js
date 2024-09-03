0f6f66538288741f3366758051667347d20136fc
Deepanagarjan <deepanagarajan1208@gmail.com>
2024-09-03 21:07:52 +0530

    updated successfully

--- Models/userSchema.js
+++ Models/userSchema.js
@@ -1,13 +1,13 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    username:String,
-    email:String,
-    password:String,
-    token:String,
-    role:String
-})
+  username: String,
+  email: String,
+  password: String,
+  token: String,
+  role: String,
+});
 
-const User = mongoose.model("User",userSchema)
+const User = mongoose.model("User", userSchema);
 
 export default User;
